3d47a8729a696b514b78a4d91e8f842bcc1ff0b3
Hannah Ahn <z5549292@ad.unsw.edu.au>
2025-10-18 18:38:32 +1100

    resolved conflicts

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
 print("hello world")
 print("this is change from children 1")
 print("this is change from children 1 - deleted change from children 2")
+print("HELLO WORLD")
 
 print("HERE")
 
 print("this is change from children 2")
 print("tis is another change from children 2")
+print("this is last change from children 1")
